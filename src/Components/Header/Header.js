df6a308b77166d378ccbb9b5ef57d827c0056e59
Tim Maupin <timmaupin@live.com>
2023-05-01 15:47:51 -0500

    set up server frontend

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+    render() {
+
+        return (
+            <>
+                <h1>Welcome to Can of Books </h1>
+
+
+            </>
+        )
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+export default Header;
