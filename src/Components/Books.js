8ea78c5ad1853d9fdae12c9a7f4fbcf727ad4304
Tim Maupin <timmaupin@live.com>
2023-05-02 15:56:28 -0500

    beginn the post and delete functions

---
+++ src/Components/Books.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Container, ListGroup, ListGroupItem } from "react-bootstrap";
+
+
+class Books extends React.Component {
+
+    render() {
+        console.log('Ripped pages', this.props.books);
+        let books = this.props.books.map((book) => (
+        <Book
+        book={book}
+        key={book._id}
+        />
+        ));
+        return (
+        <Container>
+            <ListGroup>{books}</ListGroup>
+        </Container>
+        );
+    }
+}
+
+class Book extends Books {
+    render() {
+        console.log(this.props.book._id, 'YAAAA');
+        return (
+            <ListGroupItem>
+                {this.props.book.name}
+                {/* <Button variant="success" onClick={() =} */}
+            </ListGroupItem>
+        )
+    }
+    
+    
+}
